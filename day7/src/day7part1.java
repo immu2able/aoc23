706a4af109a036ff11f78eb57a7f68519125375c
muthukumar.kb <muthukumar.kb@gmail.com>
2024-11-17 15:14:08 +0000

    in some shape

--- day7/src/day7part1.java
+++ day7/src/day7part1.java
@@ -14,26 +14,28 @@ public class day7part1 {
     private static Map<String, Long> twoPair = new HashMap<>();
     private static Map<String, Long> onePair = new HashMap<>();
     private static Map<String, Long> highCard = new HashMap<>();
+    private static Map<String, Long> values = new LinkedHashMap<>();
 
     private static Long errorMargins = 1l;
     
 
     public static void main(String[] args) {
-        String filePath = "day7\\data\\test-input.txt";
-        openFile(filePath);
+        String inputfilePath = "/home/user/aoc23/day7/data/input.txt";
+        String testInputfilePath = "/home/user/aoc23/day7/data/test-input.txt";
+        openFile(testInputfilePath);
         process();
     }
 
     private static void process() {
         /*
-         *  1. Categorize Hand to Hand Type:
-         *              Five of a Kind, 
-         *              Four of a Kind, 
-         *              Full House, 
-         *              Three of a Kind, 
-         *              Two Pair, 
-         *              One Pair, 
-         *              High Card
+         *  1. Categorize the given Hand to one of the following Hand Type:
+         *              a. Five of a Kind, 
+         *              b. Four of a Kind, 
+         *              c. Full House, 
+         *              d. Three of a Kind, 
+         *              e. Two Pair, 
+         *              f. One Pair, 
+         *              g. High Card
          *  2. Evaluate Hands with in Hand Type
          *  2. Rank Hands
          *  3. Compute total winings
@@ -45,6 +47,16 @@ public class day7part1 {
             categorizeHand(hand);
         }
         
+
+
+        sortByValue((HashMap<String, Long>) fiveOfAKind);
+        sortByValue((HashMap<String, Long>) fourOfAKind);
+        sortByValue((HashMap<String, Long>) threeOfAKind);
+        sortByValue((HashMap<String, Long>) fullHouse);
+        sortByValue((HashMap<String, Long>) twoPair);
+        sortByValue((HashMap<String, Long>) onePair);
+        sortByValue((HashMap<String, Long>) highCard);
+
         System.out.println("Five Of A Kind: " + fiveOfAKind);
         System.out.println("Four Of A Kind: " + fourOfAKind);
         System.out.println("Three Of A Kind: " + threeOfAKind);
@@ -53,13 +65,34 @@ public class day7part1 {
         System.out.println("One Pair: " + onePair);
         System.out.println("High Card: " + highCard);
         
+
+        
+
+
+        values.putAll(fiveOfAKind);
+        values.putAll(fourOfAKind);
+        values.putAll(threeOfAKind);
+        values.putAll(fullHouse);
+        values.putAll(twoPair);
+        values.putAll(onePair);
+        values.putAll(highCard);
+        
+
         Long tw= computeTotalWinings();
         System.out.println("Total Winings: " + tw);
          
     }
 
     private static Long computeTotalWinings() {
-        return 0L;
+        int len = values.size();
+        long result = 1l;
+        for(String hand : values.keySet()) {
+            long bid = hands.get(hand);
+            System.out.println("Hand: " + hand + " Bid: " + bid + " Len: " + len);
+            result = result + (bid * len);
+            len--;
+        }
+        return result;
     }
 
     private static void categorizeHand(String hand) {
@@ -71,11 +104,23 @@ public class day7part1 {
         boolean pairFound = false;
         boolean categorized = false;
 
-        for (int i = 0; i < hand.length(); i++)
-            freq[hand.charAt(i) - '2']++;            
+        for (int i = 0; i < freq.length; i++) {
+            freq[i] = 0;
+        }
+
+        for (int i = 0; i < hand.length(); i++) {
+            //System.out.println(hand.charAt(i) + " freq now " + freq[hand.charAt(i) - '2']);
+            
+            freq[hand.charAt(i) - '2']++;      
+            //System.out.println(" freq after " + freq[hand.charAt(i) - '2']);
+
+        }      
+
+        // for(int i = 0; i < 41; i++) {
+        //     System.out.println((char)(i + '2') + " " + "freq[" + i + "]: " + freq[i]);    
+        // }
 
         for(int i = 0; i < 41; i++) {
-            //System.out.println("freq[" + i + "]: " + freq[i]);    
             if (freq[i] == 5)   {
                 fiveOfAKind.put(hand, evaluateHand(hand));
                 categorized = true;
@@ -83,6 +128,7 @@ public class day7part1 {
                 fourOfAKind.put(hand, evaluateHand(hand));
                 categorized = true;
             } else if (freq[i] == 3)    {
+                
                 threeSame = true;
                 // Handle 3ofAK and FullHouse here
                 if (twoSame && !twoDiff) {
@@ -118,25 +164,82 @@ public class day7part1 {
                     pairFound = true;
                 }
             } else if (freq[i] == 1) {
-                // Handle One Pair
-                if (pairFound) {
-                    onePair.put(hand, evaluateHand(hand));
-                    categorized = true;
-                    pairFound = false;
-                }
 
                 if (!categorized && i == 40) {
                     highCard.put(hand, evaluateHand(hand));
                     categorized = true;
                 }
+            } else if (i==40 && !categorized) {
+                if (threeSame) {
+                    threeOfAKind.put(hand, evaluateHand(hand));
+                    categorized = true;
+                    threeSame = false;
+                }
+                // Handle One Pair
+                if (twoSame) {
+                    onePair.put(hand, evaluateHand(hand));
+                    categorized = true;
+                    twoSame = false;
+                }
             }
         }
+    }
 
+    public static HashMap<String, Long> sortByValue(HashMap<String, Long> hm)
+    {
+        // Create a list from elements of HashMap
+        List<Map.Entry<String, Long> > list =
+               new LinkedList<Map.Entry<String, Long> >(hm.entrySet());
+ 
+        // Sort the list
+        Collections.sort(list, new Comparator<Map.Entry<String, Long> >() {
+            public int compare(Map.Entry<String, Long> o1, 
+                               Map.Entry<String, Long> o2)
+            {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+         
+        // put data from sorted list to hashmap 
+        HashMap<String, Long> temp = new LinkedHashMap<String, Long>();
+        for (Map.Entry<String, Long> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
+        }
+        return temp;
     }
 
     private static Long evaluateHand(String hand) {
-        return 0L;
+            long score = 0;
+            char[] chars = hand.toCharArray();
+            for (char c : chars) {
+                if (Character.isDigit(c)) {
+                    int val = Integer.parseInt(c+"");
+                    score += val;
+                } else {
+                    switch (c) {
+                        case 'T':
+                            score +=  10;
+                            break;
+                        case 'J':
+                            score +=  11;
+                            break;
+                        case 'Q':
+                            score +=  12;
+                            break;
+                        case 'K':
+                            score +=  13;
+                            break;
+                        case 'A':
+                            score +=  14;
+                            break;
+                        default:
+                            break;
+                    }
 
+                }
+
+        }
+        return score;
     }
 
     private static void openFile(String filePath) {
@@ -151,9 +254,7 @@ public class day7part1 {
                 //System.out.println("hand:: " + hand);
                 String bid = myReader.next();
                 //System.out.println("bid:: " + bid);
-                if (myReader.hasNext())
-                    myReader.next(); // goto next line
-
+                
                 hands.put(hand, Long.parseLong(bid));
         
             }
