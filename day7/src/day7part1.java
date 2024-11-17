c248ccbb1878704ba72830a1c734c4d02384dd34
muthukumar.kb <muthukumar.kb@gmail.com>
2024-11-17 21:34:04 +0000

    close but not yet

--- day7/src/day7part1.java
+++ day7/src/day7part1.java
@@ -4,21 +4,21 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-enum HAND {
-    A("A", 14), K("K", 13), Q("Q", 12), J("J", 11), T("T", 10), NINE("9", 9), EIGHT("8", 8), SEVEN("7", 7), SIX("6", 6), FIVE("5", 5), FOUR("4", 4), THREE("3", 3), TWO("2", 2);
+// enum HAND {
+//     A("A", 14), K("K", 13), Q("Q", 12), J("J", 11), T("T", 10), NINE("9", 9), EIGHT("8", 8), SEVEN("7", 7), SIX("6", 6), FIVE("5", 5), FOUR("4", 4), THREE("3", 3), TWO("2", 2);
 
-    private String m_name;
-    private int m_value;
+//     private String m_name;
+//     private int m_value;
 
-    HAND(String name, int value) {
-        m_name = name;
-        m_value = value;
-    }
+//     HAND(String name, int value) {
+//         m_name = name;
+//         m_value = value;
+//     }
 
-    public int getValue() {
-        return this.m_value;
-    }
-}
+//     public int getValue() {
+//         return this.m_value;
+//     }
+// }
 
 public class day7part1 {
 
@@ -37,7 +37,7 @@ public class day7part1 {
 
     public static void main(String[] args) {
         String inputfilePath = "/home/user/aoc23/day7/data/input.txt";
-        String testInputfilePath = "/home/user/aoc23/day7/data/test-input.txt";
+        String testInputfilePath = "/home/user/aoc23/day7/data/test2.txt";
         openFile(testInputfilePath);
         process();
     }
@@ -65,13 +65,13 @@ public class day7part1 {
         
 
 
-        sortByValue((HashMap<String, Long>) fiveOfAKind);
-        sortByValue((HashMap<String, Long>) fourOfAKind);
-        sortByValue((HashMap<String, Long>) threeOfAKind);
-        sortByValue((HashMap<String, Long>) fullHouse);
-        sortByValue((HashMap<String, Long>) twoPair);
-        sortByValue((HashMap<String, Long>) onePair);
-        sortByValue((HashMap<String, Long>) highCard);
+        fiveOfAKind = sortByValue((HashMap<String, Long>) fiveOfAKind);
+        fourOfAKind = sortByValue((HashMap<String, Long>) fourOfAKind);
+        threeOfAKind = sortByValue((HashMap<String, Long>) threeOfAKind);
+        fullHouse = sortByValue((HashMap<String, Long>) fullHouse);
+        twoPair = sortByValue((HashMap<String, Long>) twoPair);
+        onePair = sortByValue((HashMap<String, Long>) onePair);
+        highCard = sortByValue((HashMap<String, Long>) highCard);
 
         System.out.println("Five Of A Kind: " + fiveOfAKind);
         System.out.println("Four Of A Kind: " + fourOfAKind);
@@ -80,14 +80,7 @@ public class day7part1 {
         System.out.println("Two Pair: " + twoPair);
         System.out.println("One Pair: " + onePair);
         System.out.println("High Card: " + highCard);
-        
-        fiveOfAKind = sort(fiveOfAKind);
-        sort(fourOfAKind);
-        sort(threeOfAKind);
-        sort(fullHouse);
-        sort(twoPair);
-        sort(onePair);
-        sort(highCard);
+
 
         values.putAll(fiveOfAKind);
         values.putAll(fourOfAKind);
@@ -103,38 +96,9 @@ public class day7part1 {
          
     }
 
-    private static HashMap<String, Long> sort(Map<String, Long> hands) {
-        HashMap<String, Long> sorted = new LinkedHashMap<>();
-        
-        
-        List<HAND> handList = new ArrayList<>();
-        for (Map.Entry<String, Long> entry : hands.entrySet()) {
-            handList.add(HAND.valueOf(entry.getKey()));
-        }
-
-        
-        Collections.sort(handList, new Comparator<HAND>() {
-            @Override
-            public int compare(HAND o1, HAND o2) {
-                return o2.getValue() - o1.getValue();
-            }
-        });
-
-        System.out.println("handlist: " + handList);
-
-        
-
-        for (Map.Entry<String, Long> entry : hands.entrySet()) {
-            sorted.put(entry.getKey(), entry.getValue());
-        }
-        System.out.println(sorted);
-
-        return sorted;
-    }
-
     private static Long computeTotalWinings() {
         int len = values.size();
-        long result = 1l;
+        long result = 0l;
         for(String hand : values.keySet()) {
             long bid = hands.get(hand);
             System.out.println("Hand: " + hand + " Bid: " + bid + " Len: " + len);
@@ -149,7 +113,6 @@ public class day7part1 {
         int[] freq = new int[41];
         boolean threeSame = false;
         boolean twoSame = false;
-        boolean twoDiff = false;
         boolean pairFound = false;
         boolean categorized = false;
 
@@ -157,139 +120,119 @@ public class day7part1 {
             freq[i] = 0;
         }
 
-        for (int i = 0; i < hand.length(); i++) {
-            //System.out.println(hand.charAt(i) + " freq now " + freq[hand.charAt(i) - '2']);
-            
+        for (int i = 0; i < hand.length(); i++) {            
             freq[hand.charAt(i) - '2']++;      
-            //System.out.println(" freq after " + freq[hand.charAt(i) - '2']);
-
         }      
 
-        // for(int i = 0; i < 41; i++) {
-        //     System.out.println((char)(i + '2') + " " + "freq[" + i + "]: " + freq[i]);    
-        // }
 
         for(int i = 0; i < 41; i++) {
             if (freq[i] == 5)   {
-                fiveOfAKind.put(hand, evaluateHand(hand));
+                fiveOfAKind.put(hand, 0l);
                 categorized = true;
             } else if (freq[i] == 4)    {
-                fourOfAKind.put(hand, evaluateHand(hand));
+                fourOfAKind.put(hand, 0l);
                 categorized = true;
             } else if (freq[i] == 3)    {
                 
                 threeSame = true;
-                // Handle 3ofAK and FullHouse here
-                if (twoSame && !twoDiff) {
-                    threeOfAKind.put(hand, evaluateHand(hand));
-                    categorized = true;
-                    threeSame = false;
-                    twoDiff = false;
-                    twoSame = false;
-                } else if (!twoSame && twoDiff) {
-                    fullHouse.put(hand, evaluateHand(hand));
-                    categorized = true;
-                    threeSame = false;
-                    twoDiff = false;
-                    twoSame = false;
-                }
-            } else if (freq[i] == 2) {
-                // Handle 3ofAK
-                twoSame = true;
-                if (threeSame) {
-                    threeOfAKind.put(hand, evaluateHand(hand));
-                    categorized = true;
-                    threeSame = false;
-                    twoDiff = false;
-                    twoSame = false;
-                }
 
+            } else if (freq[i] == 2) {
+                twoSame = true;
                 // Handle Two Pair
                 if (pairFound) {
-                    twoPair.put(hand, evaluateHand(hand));
+                    twoPair.put(hand, 0l);
                     categorized = true;
                     pairFound = false;
                 } else {
                     pairFound = true;
                 }
-            } else if (freq[i] == 1) {
-
-                if (!categorized && i == 40) {
-                    highCard.put(hand, evaluateHand(hand));
-                    categorized = true;
-                }
             } else if (i==40 && !categorized) {
-                if (threeSame) {
-                    threeOfAKind.put(hand, evaluateHand(hand));
+                if (threeSame && twoSame) {
+                    fullHouse.put(hand, 0l);
+                    categorized = true;
+                    threeSame = false;
+                    twoSame = false;
+                }
+                else if (threeSame) {
+                    threeOfAKind.put(hand, 0l);
                     categorized = true;
                     threeSame = false;
                 }
                 // Handle One Pair
-                if (twoSame) {
-                    onePair.put(hand, evaluateHand(hand));
+                else if (twoSame) {
+                    onePair.put(hand, 0l);
                     categorized = true;
                     twoSame = false;
+                } else {   
+                    highCard.put(hand, 0l);
+                    categorized = true;
                 }
             }
         }
     }
 
-    public static HashMap<String, Long> sortByValue(HashMap<String, Long> hm)
-    {
-        // Create a list from elements of HashMap
-        List<Map.Entry<String, Long> > list =
-               new LinkedList<Map.Entry<String, Long> >(hm.entrySet());
- 
-        // Sort the list
-        Collections.sort(list, new Comparator<Map.Entry<String, Long> >() {
-            public int compare(Map.Entry<String, Long> o1, 
-                               Map.Entry<String, Long> o2)
-            {
-                return (o1.getValue()).compareTo(o2.getValue());
+
+    private static int compareHands(String hand1, String hand2) {
+        String cardOrder = "AKQJT98765432"; // Define your desired card order
+        for (int i = 0; i < hand1.length(); i++) {
+            int index1 = cardOrder.indexOf(hand1.charAt(i));
+            int index2 = cardOrder.indexOf(hand2.charAt(i));
+            if (index1 != index2) {
+                return Integer.compare(index1, index2);
             }
-        });
+        }
+        return 0; // Hands are equal
+    }
+
+    private static HashMap<String, Long> sortByValue(HashMap<String, Long> hm)
+    {
+
+        List<String> handsList = new ArrayList<String>(hm.keySet());
+        
+        Collections.sort(handsList, day7part1::compareHands);
          
         // put data from sorted list to hashmap 
         HashMap<String, Long> temp = new LinkedHashMap<String, Long>();
-        for (Map.Entry<String, Long> aa : list) {
-            temp.put(aa.getKey(), aa.getValue());
+        for (String key : handsList) {
+            temp.put(key, hm.get(key));
         }
         return temp;
     }
 
-    private static Long evaluateHand(String hand) {
-            long score = 0;
-            char[] chars = hand.toCharArray();
-            for (char c : chars) {
-                if (Character.isDigit(c)) {
-                    int val = Integer.parseInt(c+"");
-                    score += val;
-                } else {
-                    switch (c) {
-                        case 'T':
-                            score +=  10;
-                            break;
-                        case 'J':
-                            score +=  11;
-                            break;
-                        case 'Q':
-                            score +=  12;
-                            break;
-                        case 'K':
-                            score +=  13;
-                            break;
-                        case 'A':
-                            score +=  14;
-                            break;
-                        default:
-                            break;
-                    }
+    // private static Long evaluateHand(String hand) {
+    //     long score = 0;
+    //     char[] chars = hand.toCharArray();
+    //     int len = 5;
+    //     for (char c : chars) {
+    //         if (Character.isDigit(c)) {
+    //             int val = Integer.parseInt(c+"");
+    //             score += (val * len );
+    //         } else {
+    //             switch (c) {
+    //                 case 'T':
+    //                     score +=  (10*len);
+    //                     break;
+    //                 case 'J':
+    //                     score +=  (100*len);
+    //                     break;
+    //                 case 'Q':
+    //                     score +=  (1000*len);
+    //                     break;
+    //                 case 'K':
+    //                     score +=  (10000*len);
+    //                     break;
+    //                 case 'A':
+    //                     score +=  (100000*len);
+    //                     break;
+    //                 default:
+    //                     break;
+    //             }
 
-                }
-
-        }
-        return score;
-    }
+    //         }
+    //         len--;
+    //     }
+    //     return score;
+    // }
 
     private static void openFile(String filePath) {
         try {
